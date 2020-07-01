20befe8e1d2ebfa809747f46ed51957eab4de083
Margo <margo.lee.smith@gmail.com>
2020-07-01 07:18:02 -0700

    add support for ascending/descending sort for all columns

--- components/table.jsx
+++ components/table.jsx
@@ -2,7 +2,7 @@ import Row from './row.jsx'
 import styles from './table.module.css'
 import keyToNameMap from '../keyToNameMap.json'
 
-function getSortedRows(data, sortBy) {
+function sortAscending(data, sortBy) {
   return data.sort((a, b) => {
     if ( a[sortBy] < b[sortBy] ) {
       return -1;
@@ -14,38 +14,67 @@ function getSortedRows(data, sortBy) {
   });
 }
 
+function sortDescending(data, sortBy) {
+  return data.sort((a, b) => {
+    if ( b[sortBy] < a[sortBy] ) {
+      return -1;
+    }
+    if ( b[sortBy] > a[sortBy] ) {
+      return 1;
+    }
+    return 0;
+  });
+}
+
 class Table extends React.Component {
   constructor(props) {
     super(props);
     this.data = props.data;
     this.state = {
-      sortBy: 'city'
+      sortBy: 'city',
+      sortAscending: false
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(event) {
     event.preventDefault();
-    this.setState({
-      sortBy: event.target.dataset.id
-    });
+    const columnClicked = event.target.dataset.id;
+    const newState = this.state;
+    newState.sortAscending = this.state.sortBy === columnClicked ?
+      !this.state.sortAscending :
+      false;
+    newState.sortBy = columnClicked;
+    this.setState(newState);
+  }
+
+  renderArrow(key) {
+    if ( this.state.sortBy === key ) {
+      return (
+        <span className={styles.arrow}>
+          {this.state.sortAscending ? '▲' : '▼'}
+        </span>
+      );
+    }
   }
 
   renderColumnHeaders() {
     return Object.keys(keyToNameMap).map(key => {
       return (
         <th key={key}
-            className={this.state.sortBy === key ? styles.sortedBy : ''}
             data-id={key}
             onClick={this.handleClick}>
           {keyToNameMap[key]}
+          {this.renderArrow(key)}
         </th>
       );
     });
   }
 
   renderRows() {
-    const sortedRows = getSortedRows(this.data, this.state.sortBy);
+    let sortedRows = this.state.sortAscending ?
+      sortAscending(this.data, this.state.sortBy) :
+      sortDescending(this.data, this.state.sortBy);
     return sortedRows.map((row, i) => <Row key={i} row={row}/>);
   }
 
