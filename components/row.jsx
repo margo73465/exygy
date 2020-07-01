e1537a4023b4aeb263c9eaed132ff9cc33a36bae
Margo <margo.lee.smith@gmail.com>
2020-06-30 20:02:20 -0700

    make responsive, remove unnecessary stylesheet

--- components/row.jsx
+++ components/row.jsx
@@ -3,12 +3,12 @@ import styles from './row.module.css'
 export default function Row({ row }) {
   return (
     <tr className={styles.row}>
-      <td>{row.city}</td>
-      <td>{row.year}</td>
-      <td>{row.median_list_rent}</td>
-      <td>{row.median_list_rent_ia}</td>
-      <td>{row.median_list_rent_percentchg_1994}</td>
-      <td>{row.median_list_rent_ia_percentchg_1994}</td>
+      <td data-label="City">{row.city}</td>
+      <td data-label="Year">{row.year}</td>
+      <td data-label="Median List Rent">{row.median_list_rent}</td>
+      <td data-label="Median List Rent (IA)">{row.median_list_rent_ia}</td>
+      <td data-label="Median List Rent (% Change From 1994)">{row.median_list_rent_percentchg_1994}</td>
+      <td data-label="Median List Rent (IA % Change From 1994)">{row.median_list_rent_ia_percentchg_1994}</td>
     </tr>
   );
 }
