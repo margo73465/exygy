c10e27a04b7761508a3747e33a4358060d0355db
Margo <margo.lee.smith@gmail.com>
2020-06-30 22:35:42 -0700

    add key to data items in each row

--- components/row.jsx
+++ components/row.jsx
@@ -1,14 +1,14 @@
 import styles from './row.module.css'
 import keyToNameMap from '../keyToNameMap.json';
 
-function renderData(data, label) {
-  return <td data-label={label}>{data}</td>;
+function renderData(data, label, index) {
+  return <td key={index} data-label={label}>{data}</td>;
 }
 
 export default function Row({ row }) {
   return (
     <tr className={styles.row}>
-      {Object.keys(keyToNameMap).map(key => renderData(row[key], keyToNameMap[key]))}
+      {Object.keys(keyToNameMap).map((key, i) => renderData(row[key], keyToNameMap[key], i))}
     </tr>
   );
 }
