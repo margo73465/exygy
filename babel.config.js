b2f81d6749a15ec4252a1876c9ae07b82c77c87a
Margo <margo.lee.smith@gmail.com>
2020-07-02 09:24:27 -0700

    use nextjs presets in babel config so we don't break the app

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets: [
+    [
+      'next/babel',
+      {
+        '@babel/preset-env': {},
+        '@babel/preset-react': {}
+      }
+    ]
+  ]
 };
